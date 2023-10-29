e114d34d6d28ae9ffb0610221815a6de0f01ff1e
Ephraim Jude <Ephraimjude1@gmail.com>
2023-10-29 19:26:31 +0100

    day 6: decided to code a calculator!!!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Day #6
+
+### CALCULATOR
+Beatifully coded calculator (if i do say so myself, lol). Several attempts later.
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot](screenshot.PNG)
---
+++ script.js
@@ -0,0 +1,86 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const numBox = document.getElementById("numBox");
+    const buttons = document.querySelectorAll(".number-button");
+    const operatorButtons = document.querySelectorAll(".operator-button");
+    const clearButton = document.getElementById("clear");
+    const plusMinusButton = document.getElementById("posOrNeg");
+
+    let currentInput = "0";
+
+    function updateDisplay() {
+        numBox.value = formatInput(currentInput);
+        updatePlusMinusButton();
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            handleNumberButtonClick(button.value);
+        });
+    });
+
+    operatorButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+            handleOperatorButtonClick(button.value);
+        });
+    });
+
+    clearButton.addEventListener("click", function () {
+        clearDisplay();
+    });
+
+    plusMinusButton.addEventListener("click", function () {
+        togglePlusMinus();
+    });
+
+    function handleNumberButtonClick(value) {
+        if (currentInput === "0" || currentInput === "Error") {
+            currentInput = value;
+        } else if (currentInput.length > 0) { 
+            currentInput += value;
+        }
+        updateDisplay();
+    }
+
+    function handleOperatorButtonClick(value) {
+        if (value === "=") {
+            evaluateExpression();
+        } else {
+            currentInput += ` ${value} `;
+            updateDisplay();
+        }
+    }
+
+    function evaluateExpression() {
+        try {
+            currentInput = eval(currentInput);
+            if (isFinite(currentInput)) {
+                currentInput = currentInput.toFixed(4); // Display up to 6 decimal places
+            }
+        } catch (error) {
+            currentInput = "Error";
+        }
+        updateDisplay();
+    }
+
+    function clearDisplay() {
+        currentInput = "0";
+        updateDisplay();
+    }
+
+    function togglePlusMinus() {
+        currentInput = -currentInput;
+        updateDisplay();
+    }
+
+    function updatePlusMinusButton() {
+        plusMinusButton.disabled = currentInput === "0" || currentInput === "Error";
+    }
+
+    // Function to format the input
+    function formatInput(input) {
+        if (isFinite(input)) {
+            return parseFloat(input).toString(); // Remove leading zeros
+        }
+        return input;
+    }
+});
